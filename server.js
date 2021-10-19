3cbcf9c977ebe93854cddb0ea59136eb01af6684
Paul <lambdapaul@protonmail.com>
2021-10-18 23:05:32 -0400

    Add basic server functionalities

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;  
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+///////////////////////////////////////////////////
+// For Heroku deployment
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') 
+{  
+    // Set static folder  
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) =>  
+{    
+res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')); 
+ });}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,7 +39,7 @@ app.use((req, res, next) => 
   next();
 });
 
-app.listen(5000); // start Node + Express server on port 5000
+app.listen(PORT, () => {  console.log('Server listening on port ' + PORT);});
 
 var cardList =
 [
@@ -124,6 +143,12 @@ var cardList =
    'Rickey Henderson',
    'Babe Ruth'
  ];
+
+ require('dotenv').config();
+ const url = process.env.MONGODB_URI;
+ const MongoClient = require('mongodb').MongoClient; 
+const client = new MongoClient(url);
+ client.connect();
 
  app.post('/api/addcard', async (req, res, next) =>
  {
