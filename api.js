a9af2c13f3d1dceb61013756fcd37417853d9be7
Paul <lambdapaul@protonmail.com>
2021-10-25 23:06:36 -0400

    Switch from commonjs to ES modules

--- api.js
+++ api.js
@@ -1,149 +1,197 @@
-exports.setApp = function ( app, client )
-{
-    //load user model
-    const User = require("./models/user.js");
-    //load card model
-    const Card = require("./models/card.js");
+import User from "./models/user.js";
+import Card from "./models/card.js";
+import token from "./createJWT.js";
 
-   
+function setApp (app, client) {
 
-      app.post('/api/addcard', async (req, res, next) =>
-      {
-        // incoming: userId, color
-        // outgoing: error
-          
-        const { userId, card, jwtToken } = req.body;
-        try
-        {
-          if( token.isExpired(jwtToken))
-          {
-            var r = {error:'The JWT is no longer valid', jwtToken: ''};
-            res.status(200).json(r);
-            return;
-          }
-        }
-        catch(e)
-        {
-          console.log(e.message);
-        }
-      
-        //const newCard = { Card: card, UserId: userId };
-        const newCard = new Card({ Card: card, UserId: userId });
-        var error = '';
-        try 
-        {
-            // const db = client.db();
-            // const result = db.collection('Cards').insertOne(newCard);
-            newCard.save();
-        }
-        catch (e) 
-        {
-            error = e.toString();
-        }
-      
-        var refreshedToken = null;
-        try
-        {
-          refreshedToken = token.refresh(jwtToken);
-        }
-        catch(e)
-        {
-          console.log(e.message);
-        }
-      
-        var ret = { error: error, jwtToken: refreshedToken };
-        
-        res.status(200).json(ret);
-      });
-    
-    app.post('/api/login', async (req, res, next) => 
-    {
-      // incoming: login, password
-      // outgoing: id, firstName, lastName, error
-    
-     var error = '';
-    
-     const { login, password } = req.body;
-     // const db = client.db();
-     // const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-     const results = await User.find({ Login: login, Password: password });
+    app.post(
+        "/api/addcard",
+        async (req, res, next) => {
 
-    db.collection('Users').find({Login:login,Password:password}).toArray();
-    
-      var id = -1;
-      var fn = '';
-      var ln = '';
-      var ret;
-    
-      if( results.length > 0 )
-      {
-        id = results[0].UserId;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-        try
-        {
-          const token = require("./createJWT.js");
-          ret = token.createToken( fn, ln, id );
+            /*
+             * Incoming: userId, color
+             * outgoing: error
+             */
+
+            const {userId, card, jwtToken} = req.body;
+            try {
+
+                if (token.isExpired(jwtToken)) {
+
+                    const r = {"error": "The JWT is no longer valid",
+                        "jwtToken": ""};
+                    res.status(200).json(r);
+                    return;
+
+                }
+
+            } catch (e) {
+
+                console.log(e.message);
+
+            }
+
+            // Const newCard = { Card: card, UserId: userId };
+            const newCard = new Card({"Card": card,
+                "UserId": userId});
+            let error = "";
+            try {
+
+                /*
+                 * Const db = client.db();
+                 * const result = db.collection('Cards').insertOne(newCard);
+                 */
+                newCard.save();
+
+            } catch (e) {
+
+                error = e.toString();
+
+            }
+
+            let refreshedToken = null;
+            try {
+
+                refreshedToken = token.refresh(jwtToken);
+
+            } catch (e) {
+
+                console.log(e.message);
+
+            }
+
+            const ret = {error,
+                "jwtToken": refreshedToken};
+
+            res.status(200).json(ret);
+
         }
-        catch(e)
-        {
-          ret = {error:e.message};
+    );
+
+    app.post(
+        "/api/login",
+        async (req, res, next) => {
+
+            /*
+             * Incoming: login, password
+             * outgoing: id, firstName, lastName, error
+             */
+
+            const error = "",
+
+                {login, password} = req.body,
+
+                /*
+                 * Const db = client.db();
+                 * Const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+                 */
+                results = await User.find({"Login": login,
+                    "Password": password});
+
+            db.collection("Users").find({"Login": login,
+                "Password": password}).
+                toArray();
+
+            let id = -1,
+                fn = "",
+                ln = "",
+                ret;
+
+            if (results.length > 0) {
+
+                id = results[0].UserId;
+                fn = results[0].FirstName;
+                ln = results[0].LastName;
+                try {
+
+                    ret = token.createToken(
+                        fn,
+                        ln,
+                        id
+                    );
+
+                } catch (e) {
+
+                    ret = {"error": e.message};
+
+                }
+
+            } else {
+
+                ret = {"error": "Login/Password incorrect"};
+
+            }
+
+            res.status(200).json(ret);
+
         }
-      }
-      else
-      {
-          ret = {error:"Login/Password incorrect"};
-      }
-    
-      res.status(200).json(ret);
-    });
-    
-    app.post('/api/searchcards', async (req, res, next) => 
-    {
-      // incoming: userId, search
-      // outgoing: results[], error
-    
-      var error = '';
-    
-      const { userId, search, jwtToken } = req.body;
-      try
-      {
-        if( token.isExpired(jwtToken))
-        {
-          var r = {error:'The JWT is no longer valid', jwtToken: ''};
-          res.status(200).json(r);
-          return;
+    );
+
+    app.post(
+        "/api/searchcards",
+        async (req, res, next) => {
+
+            /*
+             * Incoming: userId, search
+             * outgoing: results[], error
+             */
+
+            const error = "",
+
+                {userId, search, jwtToken} = req.body;
+            try {
+
+                if (token.isExpired(jwtToken)) {
+
+                    const r = {"error": "The JWT is no longer valid",
+                        "jwtToken": ""};
+                    res.status(200).json(r);
+                    return;
+
+                }
+
+            } catch (e) {
+
+                console.log(e.message);
+
+            }
+
+            const _search = search.trim(),
+
+                /*
+                 *   Const db = client.db();
+                 *   Const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*', $options: 'r' } }).toArray();
+                 */
+                results = await Card.find({"Card": {"$regex": `${_search}.*`,
+                    "$options": "r"}}),
+
+                _ret = [];
+            for (let i = 0; i < results.length; i++) {
+
+                _ret.push(results[i].Card);
+
+            }
+
+            let refreshedToken = null;
+            try {
+
+                refreshedToken = token.refresh(jwtToken);
+
+            } catch (e) {
+
+                console.log(e.message);
+
+            }
+
+            const ret = {"results": _ret,
+                error,
+                "jwtToken": refreshedToken};
+
+            res.status(200).json(ret);
+
         }
-      }
-      catch(e)
-      {
-        console.log(e.message);
-      }
-      
-      var _search = search.trim();
-      //   const db = client.db();
-      //   const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*', $options: 'r' } }).toArray();
-      const results = await Card.find({ "Card": { $regex: _search + '.*', $options: 'r' } });
-      
-      var _ret = [];
-      for( var i=0; i<results.length; i++ )
-      {
-        _ret.push( results[i].Card );
-      }
-      
-      var refreshedToken = null;
-      try
-      {
-        refreshedToken = token.refresh(jwtToken);
-      }
-      catch(e)
-      {
-        console.log(e.message);
-      }
-    
-      var ret = { results:_ret, error: error, jwtToken: refreshedToken };
-      
-      res.status(200).json(ret);
-    });
-    
+    );
+
 }
+
+export default {setApp};
--- createJWT.js
+++ createJWT.js
@@ -1,53 +1,79 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-exports.createToken = function ( fn, ln, id )
-{
-    return _createToken( fn, ln, id );
-}
-_createToken = function ( fn, ln, id )
-{
-    try
-    {
-      const expiration = new Date();
-      const user = {userId:id,firstName:fn,lastName:ln};
-      const accessToken =  jwt.sign( user, process.env.ACCESS_TOKEN_SECRET);
-      // In order to exoire with a value other than the default, use the 
-       // following
-      /*
-      const accessToken= jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, 
-         { expiresIn: '30m'} );
-                       '24h'
-                      '365d'
-      */
-      var ret = {accessToken:accessToken};
-    }
-    catch(e)
-    {
-      var ret = {error:e.message};
+import jwt from "jsonwebtoken";
+import {config as dotenvConfig} from "dotenv";
+
+dotenvConfig();
+
+function createToken (fn, ln, id) {
+
+    let ret;
+    try {
+
+        const expiration = new Date(),
+            user = {"userId": id,
+                "firstName": fn,
+                "lastName": ln},
+            accessToken = jwt.sign(
+                user,
+                process.env.ACCESS_TOKEN_SECRET
+            );
+
+        /*
+         * In order to exoire with a value other than the default, use the
+         * following
+         */
+        /*
+         *Const accessToken= jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,
+         *   { expiresIn: '30m'} );
+         *                 '24h'
+         *                '365d'
+         */
+        ret = {accessToken};
+
+    } catch (e) {
+
+        ret = {"error": e.message};
+
     }
     return ret;
+
 }
-exports.isExpired = function( token )
-{
-   var isError = jwt.verify( token, process.env.ACCESS_TOKEN_SECRET, 
-     (err, verifiedJwt) =>
-   {
-     if( err )
-     {
-       return true;
-     }
-     else
-     {
-       return false;
-     }
-   });
-   return isError;
+function isExpired (token) {
+
+    const isError = jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, verifiedJwt) => {
+
+            if (err) {
+
+                return true;
+
+            }
+            return false;
+
+        }
+    );
+    return isError;
+
 }
-exports.refresh = function( token )
-{
-  var ud = jwt.decode(token,{complete:true});
-  var userId = ud.payload.id;
-  var firstName = ud.payload.firstName;
-  var lastName = ud.payload.lastName;
-  return _createToken( firstName, lastName, userId );
+
+function refresh (token) {
+
+    const ud = jwt.decode(
+            token,
+            {"complete": true}
+        ),
+        userId = ud.payload.id,
+        {firstName} = ud.payload,
+        {lastName} = ud.payload;
+    return createToken(
+        firstName,
+        lastName,
+        userId
+    );
+
 }
+
+export default {refresh,
+    isExpired,
+    createToken};
