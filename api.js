e47676561a1857759ae812f61ff185b4e2c9f0ed
Joel Cruz <joel_cruz410@yahoo.com>
2021-11-09 15:37:49 -0500

    Test API's (may not work)

--- api.js
+++ api.js
@@ -16,53 +16,122 @@ function setApp (app, client) {
 
                 {login, password} = req.body,
 
-                /*
-                 * Const db = client.db();
-                 * Const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-                 */
-                results = await User.find({"Login": login,
-                    "Password": password});
+                
+                  const db = client.db();
+                  const results = await db.collection('Users').findOne({
+                      $and: [{Password: password},
+                      {$or: [{Email: login}, {Login: login}]}]}
+                  )
+                 
+                  var id = -1;  
+                  var fn = '';  
+                  var ln = '';
+                  var un = '';  
+                  var verStatus = 0;
+                  if( results )  
+                  {    
+                      id = results._id;    
+                      fn = results.FirstName;    
+                      ln = results.LastName;
+                      un = results.Login;
+                      verStatus = results.AuthStatus;
+                      if (verStatus == 0)
+                      {
+                          ret = {error:"Account not verified."};
+                      }
+                      else
+                      {
+                          try        
+                          {          
+                              const token = require("./createJWT.js");          
+                              ret = token.createToken( fn, ln, un, id );        
+                          }        
+                          catch(e)        
+                          {          
+                              ret = {error:e.message};        
+                          }  
+                      }  
+              
+                  }      
+                  else      
+                  {          
+                      ret = {error:"Login/Password incorrect."};      
+                  }
+                  res.status(200).json(ret);
+              });
 
-            db.collection("Users").find({"Login": login,
-                "Password": password}).
-                toArray();
+              app.post('/api/forgotpassword', async( req, res, next) =>
+              {
+                const {email} = req.body;
+                const db = client.db();
+                db.collection('Users').findOne({Email: email}, (err, user) =>
+                {
+                    if(err || !user)
+                    {
+                        return res.status(400).json({error:"Account with email does not exist."});
+                    }
 
-            let id = -1,
-                fn = "",
-                ln = "",
-                ret;
+                    const jwtoken = jwt.sign({_id: user._id}, process.env.RESET_PASSWORD_KEY, {expiresIn: '20m'});
+                    // todo: do this as html
+                    var message = "Hello, this is a password reset request for your Todo account! If you did not request this, please ignore. Reset your password here: https://cop4331-test123.herokuapp.com/";
+                    message += jwtoken;
+                    const checking = sendEmail.sendEmail(email, "Todo: Password Reset Request", message);
 
-            if (results.length > 0) {
-
-                id = results[0].UserId;
-                fn = results[0].FirstName;
-                ln = results[0].LastName;
-                try {
-
-                    ret = token.createToken(
-                        fn,
-                        ln,
-                        id
+                    db.collection('Users').updateOne(
+                        {_id:user._id},
+                        {
+                            $set: {resetPassword: jwtoken}  
+                        }
                     );
+            
+                    return res.status(200).json({error: ""});
 
-                } catch (e) {
+                })
 
-                    ret = {"error": e.message};
+            });
 
-                }
+              app.post('/api/resetpassword', async( req, res, next) =>
+              {
+                  const {resetLink, newPassword} = req.body;
+                  const db = client.db();
+                  if(resetLink)
+                  {
+                      jwt.verify(resetLink, process.env.RESET_PASSWORD_KEY, function(err, decodedData)
+                      {
+                          if(err)
+                          {
+                              return res.status(401).json({error:"Invalid token (either incorrect or expired)."});
+                          }
+                          db.collection('Users').findOne({resetPassword: resetLink}, (err, user) =>
+                          {
+                              if(err || !user)
+                              {
+                                  return res.status(400).json({error: "Invalid token."});
+                              }
+                              else
+                              {
+                                  db.collection('Users').updateOne(
+                                      {_id:user._id},
+                                      {
+                                          $set: {Password: newPassword}
+          
+                                      }
+                                  )
+                                  return res.status(200).json({error: ""});
+                              }
+          
+                          })
+          
+                      })
+                  }
+                  else
+                  {
+                      return res.status(401).json({error: "Authentication error."});
+                  }
+          
+              });          
 
-            } else {
-
-                ret = {"error": "Login/Password incorrect"};
-
-            }
-
-            res.status(200).json(ret);
-
-        }
-    );
-
-    app.create(
+    app.post(
         "/api/create",
         async (req, res, next) => {
             // test api for create
@@ -73,21 +142,21 @@ function setApp (app, client) {
         }
     );
 
-    app.get(
+    app.post(
         "/api/read",
         async (req, res, next) => {
             // test api for read
         }
     );
 
-    app.update(
+    app.post(
         "/api/update",
         async(req, res, next) => {
             // test api for update
         }
     );
     
-    app.delete(
+    app.post(
         "/api/delete",
         async (req, res, next) => {
             // test api for delete
