8ed55a202189de951c1c840137edcd4c5110f11a
manahilA <manahilawan@knights.ucf.edu>
2021-11-21 18:44:30 -0500

    Working on to-do list
    - Added navigation/routing to tabs.js
    - Removed headers
    - Added task component
    - Working on to-do list functionality, skeleton in place

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Tabs from './navigation/tabs';
 
+import HomeScreen from './screens/HomeScreen';
+import RegistrationScreen from './screens/RegistrationScreen';
+
 const App = () => {
+  console.log("App execeuted");
   return (
-    <NavigationContainer>
-      <Tabs />
+    <NavigationContainer>   
+      <Tabs/>
     </NavigationContainer>
   );
 }
