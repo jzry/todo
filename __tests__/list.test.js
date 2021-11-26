f2ccbfeeab7b2e66c92ef7d51460bb679216f3e0
Paul <lambdapaul@protonmail.com>
2021-11-26 00:59:12 -0500

    Finish list unit tests

--- __tests__/list.test.js
+++ __tests__/list.test.js
@@ -1,9 +1,8 @@
 const axios = require("axios");
+const dotenv = require("dotenv");
 
-const login = {
-    login: "test",
-    password: "password"
-}
+dotenv.config();
+const PORT = process.env.PORT || 8080;
 
 function getToken() {
     return new Promise((resolve, reject) => {
@@ -14,7 +13,10 @@ function getToken() {
             headers: {
                 "Content-Type": "application/json"
             },
-            data: login
+            data: {
+                login: "test",
+                password: "password"
+            }
         };
 
         axios(config)
@@ -35,73 +37,109 @@ function getToken() {
     });
 }
 
-describe("POST /api/lists", () => {
+const title = `${Math.random() % 100}-title`;
+const updatedTitle = `${Math.random() % 100}-title-updated`;
 
+function readTestList(title) {
+    return new Promise(async (resolve, reject) => {
+        let token;
+        try {
+            token = await getToken();
+        }
+         catch(e) {
+            reject(e);
+         }
+
+        const config = {
+            method: "post",
+            url: `http://localhost:${PORT}/api/lists/read`,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: {
+                token: token,
+                search: title
+            }
+        };
+
+        axios(config)
+            .then(function (response) {
+                const res = response.data;
+                if (res.error) {
+                    reject(res.error);
+                    return;
+                }
+                resolve(res[0]?.id);
+
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    reject(error.response.data);
+                    return;
+                }
+                reject(error);
+            });
+        }); 
+}
+
+
+describe("Test /api/lists endpoints", () => {
     describe("Create a list", () => {
         // create a list in the database
         // should respond with a json object containing the list id
-        // should respond with a 200 status code
+        // change this please :)
 
-        test("should respond with a 200 status code", async () => {
-            let token
+        test("creates a new list with a randomly generated title", async () => {
+            let token;
             try {
-
                 token = await getToken();
-
-            } catch(e) {
-                done(e);
             }
+             catch(e) {
+                 expect(e).toBeUndefined();
+             }
 
             const config = {
                 method: "post",
-                url: "http://localhost:8090/api/lists/create",
+                url: `http://localhost:${PORT}/api/lists/create`,
                 headers: {
                     "Content-Type": "application/json"
                 },
                 data: {
                     token: token,
-                    title: "Sample",
+                    title: title,
                     list: []
                 }
             };
-
 
             axios(config)
                 .then(async function (response) {
                     const res = response.data;
                     if (res.error) {
-                        console.error(res.error);
-                        return;
+                        expect(res.error).toBeUndefined();
                     }
 
                     expect(res.id).toBeDefined();
 
                 })
                 .catch(function (error) {
-                    if (error.response) {
-                        console.error(error.response.data);
-                        return;
-                    }
-                    console.error(error);
+                    expect(error).toBeUndefined();
                 });
         });
     })
 
-    describe("Read the list", () => {
-
-        test("should respond with a 200 status code", async () => {
-            let token
+    describe("Read lists", () => {
+        test("reads all the lists associated with the test user", async () => {
+            let token;
             try {
-
                 token = await getToken();
-
-            } catch(e) {
-                done(e);
             }
+             catch(e) {
+                 expect(e).toBeUndefined();
+             }
 
             const config = {
                 method: "post",
-                url: "http://localhost:8090/api/lists/read",
+                url: `http://localhost:${PORT}/api/lists/read`,
                 headers: {
                     "Content-Type": "application/json"
                 },
@@ -114,33 +152,92 @@ describe("POST /api/lists", () => {
             axios(config)
                 .then(function (response) {
                     const res = response.data;
-                    // const res = undefined;
                     if (res.error) {
-                        console.error(res.error);
-                        return;
+                        expect(res.error).toBeUndefined();
                     }
-
                     expect(res).toBeDefined();
 
                 })
                 .catch(function (error) {
-                    if (error.response) {
-                        console.error(error.response.data);
-                        return;
-                    }
-                    console.error(error);
+                    expect(error).toBeUndefined();
                 });
+            }); 
         });
 
-    })
-    describe("update the list", () => {
-        // updates the list in the database
-        // should respond with a 200 status and update the list
-    })
+    describe("update the list",  () => {
+        test("updates the title to a randomly generated title", async () => {
+            let id, token;
+            try {
+                id = await readTestList(title);
+                token = await getToken();
+            }
+             catch(e) {
+                 expect(e).toBeUndefined();
+             }
+
+        const config = {
+            method: "post",
+            url: `http://localhost:${PORT}/api/lists/update`,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: {
+                id: id,
+                token: token,
+                title: updatedTitle,
+                list: []
+            }
+        };
+    
+
+        axios(config)
+            .then(async function (response) {
+                const res = response.data;
+                if (res.error) {
+                    expect(res.error).toBeUndefined();
+                }
+                expect(res.message).toEqual("list update success");
+            })
+            .catch(function (error) {
+                expect(error).toBeUndefined();
+            });
+        });
+    });
 
     describe("deletes the list", () => {
-        // deletes the list from the database
-        // should respond with a 200 status and deletes the list
+        test("delete a list with a specific id", async () => {
+            let id, token;
+            try {
+                 id = await readTestList(updatedTitle);
+                 token = await getToken();
+            } catch(e) {
+                expect(e).toBeUndefined();
+            }
+    
+            const config = {
+                method: "post",
+                url: `http://localhost:${PORT}/api/lists/delete`,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: {
+                    id: id,
+                    token: token,
+                }
+            };
         
-    })
+    
+            axios(config)
+                .then(async function (response) {
+                    const res = response.data;
+                    if (res.error) {
+                        expect(res.error).toBeUndefined();
+                    }
+                    expect(res.message).toEqual("list was deleted successfully");
+                })
+                .catch(function (error) {
+                    expect(error).toBeUndefined();
+                });
+            });
+    });
 })
